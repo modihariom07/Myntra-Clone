72e3c06db3b883e2c0a1c35c78436fd8761ac24b
Hariom Modi <modihariom1212@gmail.com>
2026-08-12 00:59:41 +0530

    Update App.jsx

--- myntra-react-clone/src/routes/App.jsx
+++ myntra-react-clone/src/routes/App.jsx
@@ -2,7 +2,7 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import FetchItems from "../Components/FetchItems";
+import FetchItems from "../components/FetchItems";
 import { useSelector } from "react-redux";
 import LoadingSpinner from "../components/LoadingSpinner";
 
